bc42fb4e44a722ceef0a76fa477f3dfc9561a0de
wangyou <289582304@qq.com>
2023-09-08 13:50:10 +0800

    refactor: 重构isTypings文件代码

--- src/utils/isTypings.ts
+++ src/utils/isTypings.ts
@@ -1,3 +1,5 @@
+export const isDate = (value: unknown): value is Date =>
+  objProtoCallType(value, 'date');
 export const isObject = (value: unknown): value is Record<any, any> =>
   objProtoCallType(value, 'object');
 export const isSet = (value: unknown): value is Set<any> =>
@@ -6,7 +8,8 @@ export const isMap = (value: unknown): value is Map<any, any> =>
   objProtoCallType(value, 'map');
 export const isNull = (value: unknown): value is null =>
   objProtoCallType(value, 'null');
-export const isArray = (value: unknown): value is any[] => Array.isArray(value);
+export const isBigint = (value: unknown): value is bigint =>
+  typeof value === 'bigint';
 export const isSymbol = (value: unknown): value is symbol =>
   typeof value === 'symbol';
 export const isFunction = (value: unknown): value is Function =>
