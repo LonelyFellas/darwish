808f8688250c8ea28eeb16bec58490f68139fc8f
wangyou <289582304@qq.com>
2023-12-13 22:45:16 +0800

    feat: add two hooks(useEffectOnce...)

--- src/utils/obj/index.ts
+++ src/utils/obj/index.ts
@@ -1,8 +1,15 @@
+import { isObject } from 'darwish';
+
 export default class ExtendObject extends Object {
   static pick = <T extends Record<PropertyKey, any>>(
     dataSource: T,
     pickKeys: (keyof T)[],
   ) => {
+    if (isObject(dataSource)) {
+      console.error('DataSource 不是一个Object');
+    }
+
+    // TODO 可能要判断datasource 进来是否是一个object类型
     const obj: Partial<T> = {};
     if (Array.isArray(pickKeys)) {
       pickKeys?.forEach((key) => {
@@ -19,6 +26,10 @@ export default class ExtendObject extends Object {
     dataSource: T,
     omitKeys: (keyof T)[],
   ) => {
+    if (isObject(dataSource)) {
+      console.error('DataSource 不是一个Object');
+    }
+
     const obj: Partial<T> = {};
     if (Array.isArray(omitKeys)) {
       Object.keys(dataSource).forEach((key: keyof T) => {
