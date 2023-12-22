a381ebdd17fbc2fd97c4233b0b13b3f511ccba78
worksapce <289582304@qq.com>
2023-12-22 21:49:16 +0800

    refactor: convert some ts file into some js file

--- test/utils/deepEqual.test.js
+++ test/utils/deepEqual.test.js
@@ -1,35 +1,27 @@
-import { isSymbol } from '../../src/utils/is';
+import {isSymbol} from '../../src/utils/is';
 import deepEqual from '../../src/utils/deepEqual';
 
 
-function tag(obj: any, value: any) {
-  if (isSymbol && Symbol.toStringTag && Object.defineProperty) {
-    Object.defineProperty(obj, Symbol.toStringTag, {
-      value: value
-    });
-  }
-  return obj;
-}
 describe('deepEqual', () => {
   it('two objects', () => {
-    expect(deepEqual({ a: [2, 3], b: [4] }, { a: [2, 3], b: [4] })).toBe(true);
+    expect(deepEqual({a: [2, 3], b: [4]}, {a: [2, 3], b: [4]})).toBe(true);
     expect(
       deepEqual(
-        { a: [1, 2, 3, 4], b: 2, c: 3, d: 4, e: 5, f: 6, g: 7 },
-        { a: [1, 2, 3, 4], b: 2, c: 3, d: 4, e: 5, f: 6, g: 7 },
+        {a: [1, 2, 3, 4], b: 2, c: 3, d: 4, e: 5, f: 6, g: 7},
+        {a: [1, 2, 3, 4], b: 2, c: 3, d: 4, e: 5, f: 6, g: 7},
       ),
     ).toBe(true);
-    const obj1 = { a: [2, 3], b: [4] };
-    const obj2 = { b: [4], a: [2, 3] };
+    const obj1 = {a: [2, 3], b: [4]};
+    const obj2 = {b: [4], a: [2, 3]};
     expect(deepEqual(obj1, obj2)).toBe(true);
     // object.keys
     expect(deepEqual(Object.keys(obj1), Object.keys(obj2))).toBe(false);
     expect(deepEqual(Object.keys(obj1), Object.keys(obj2).reverse())).toBe(
       true,
     );
-    expect(deepEqual({ a: 2, b: 4 }, { a: 2, B: 4 })).toBe(false);
-    expect(deepEqual({ a: 2, b: 4 }, { a: 2, b: '4' })).toBe(false);
-    expect(deepEqual({ a: 2, b: 0 }, { a: 2, b: false })).toBe(false);
+    expect(deepEqual({a: 2, b: 4}, {a: 2, B: 4})).toBe(false);
+    expect(deepEqual({a: 2, b: 4}, {a: 2, b: '4'})).toBe(false);
+    expect(deepEqual({a: 2, b: 0}, {a: 2, b: false})).toBe(false);
   });
   it('two Map', () => {
     expect(
@@ -65,21 +57,20 @@ describe('deepEqual', () => {
     expect(
       deepEqual(
         new Map([[undefined, undefined]]),
-        // @ts-ignore
+
         new Map([[undefined, null]]),
       ),
     ).toBe(false);
 
     expect(
       deepEqual(
-        // @ts-ignore
         new Map([
           [{}, null],
           [true, 2],
           [{}, 1],
           [undefined, {}],
         ]),
-        // @ts-ignore
+
         new Map([
           [{}, 1],
           [true, 2],
@@ -91,13 +82,12 @@ describe('deepEqual', () => {
 
     expect(
       deepEqual(
-        // @ts-ignore
         new Map([
           [false, 3],
           [{}, 2],
           [{}, 1],
         ]),
-        // @ts-ignore
+
         new Map([
           [{}, 1],
           [{}, 2],
@@ -109,7 +99,7 @@ describe('deepEqual', () => {
     expect(
       deepEqual(
         new Map([[null, undefined]]),
-        // @ts-ignore
+
         new Map([[null, null]]),
       ),
     ).toBe(false);
@@ -117,20 +107,19 @@ describe('deepEqual', () => {
     expect(
       deepEqual(
         new Map([[undefined, 3]]),
-        // @ts-ignore
+
         new Map([[null, 3]]),
       ),
     ).toBe(false);
 
     expect(
       deepEqual(
-        // @ts-ignore
         new Map([
           [false, 3],
           [{}, 2],
           [{}, 1],
         ]),
-        // @ts-ignore
+
         new Map([
           [{}, 1],
           [{}, 2],
@@ -143,12 +132,11 @@ describe('deepEqual', () => {
 
     expect(
       deepEqual(
-        // @ts-ignore
         new Map([
           [{}, null],
           [false, 3],
         ]),
-        // @ts-ignore
+
         new Map([
           [{}, null],
           [true, 2],
@@ -158,12 +146,11 @@ describe('deepEqual', () => {
 
     expect(
       deepEqual(
-        // @ts-ignore
         new Map([
           [false, 3],
           [{}, null],
         ]),
-        // @ts-ignore
+
         new Map([
           [true, 2],
           [{}, null],
@@ -172,12 +159,11 @@ describe('deepEqual', () => {
     ).toBe(false);
     expect(
       deepEqual(
-        // @ts-ignore
         new Map([
           [undefined, null],
           ['+000', 2],
         ]),
-        // @ts-ignore
+
         new Map([
           [null, undefined],
           [false, '2'],
@@ -194,49 +180,49 @@ describe('deepEqual', () => {
       deepEqual(new Set([{}, 1, {}, {}, 2]), new Set([{}, 1, {}, 2, {}])),
     ).toBe(true);
     expect(deepEqual(new Set(), new Set([1]))).toBe(false);
-    // @ts-ignore
-    expect(deepEqual(new Set([{ a: 1 }, 2]), new Set(['2', { a: 2 }]))).toBe(
+
+    expect(deepEqual(new Set([{a: 1}, 2]), new Set(['2', {a: 2}]))).toBe(
       false,
     );
     expect(
       deepEqual(
         new Set([null, '', 1, 5, 2, false]),
-        // @ts-ignore
+
         new Set([undefined, 0, '5', true, '2', '-000']),
       ),
     ).toBe(false);
     // TODO: wait fix
-    // // @ts-ignore
+    //
     // expect(deepEqual(new Set(), new Map())).toBe(false);
     // let maplikeSet = new Set();
     // Object.defineProperty(maplikeSet, 'constructor', {
     //   enumerable: false,
     //   value: Map,
     // });
-    // // @ts-ignore
+    //
     // maplikeSet.__proto__ = Map.prototype; // eslint-disable-line no-proto
-    // // @ts-ignore
+    //
     // expect(deepEqual(maplikeSet, new Map())).toBe(false);
   });
   it('not equal', () => {
-    expect(deepEqual({ x: 5, y: [6] }, { x: 5, y: 6 })).toBe(false);
+    expect(deepEqual({x: 5, y: [6]}, {x: 5, y: 6})).toBe(false);
   });
   it('nested nulls', () => {
     expect(deepEqual([null, null, null], [null, null, null])).toBe(true);
   });
   it('objects with strings vs numbers', () => {
-    expect(deepEqual([{ a: 3 }, { b: 4 }], [{ a: '3' }, { b: '4' }])).toBe(
+    expect(deepEqual([{a: 3}, {b: 4}], [{a: '3'}, {b: '4'}])).toBe(
       false,
     );
   });
   it('non-objects', () => {
     expect(deepEqual(3, 3)).toBe(true);
     expect(deepEqual('beep', 'beep')).toBe(true);
-    // @ts-ignore
+
     expect(deepEqual('3', 3)).toBe(false);
-    // @ts-ignore
+
     expect(deepEqual('3', [3])).toBe(false);
-    // @ts-ignore
+
     expect(deepEqual(3, [3])).toBe(false);
   });
   it('infinities', () => {
@@ -249,13 +235,11 @@ describe('deepEqual', () => {
     function getArgs() {
       return arguments;
     }
-    // @ts-ignore
+
     expect(deepEqual(getArgs(1, 2, 3), getArgs(1, 2, 3))).toBe(true);
-    // @ts-ignore
+
     expect(deepEqual(getArgs(1, 2, 3), [1, 2, 4])).toBe(false);
 
-    let args = getArgs();
-    var noArgs = tag
   });
 });
 
