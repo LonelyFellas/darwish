89f7f24340aa60ae1eb0f5a990e80a4a0af0d043
worksapce <289582304@qq.com>
2023-12-30 16:24:11 +0800

    refactor: overwrite pick and omit functions

--- src/utils/obj/index.ts
+++ src/utils/obj/index.ts
@@ -1,55 +1,79 @@
 import { isArray, isBlanks, isObject } from '../is';
 
 export default class ExtendObject extends Object {
-  static pick = <T extends Darwish.AnyObj>(
+  /**
+   * @description pick keys from dataSource 
+   * @param dataSource data source 
+   * @param pickKeys pick keys 
+   * @returns picked object 
+   */
+  static pick = <T extends Darwish.AnyObj, const K extends keyof T>(
     dataSource: T,
-    pickKeys: (keyof T)[],
-  ) => {
+    pickKeys: K[],
+  ): Pick<T, K> => {
     if (!isObject(dataSource)) {
-      console.error('DataSource 不是一个Object');
+      console.error('DataSource is not an `object`');
+      return dataSource;
+    }
+    if (!isArray(pickKeys)) {
+      console.error(
+        `PickKeys: ${JSON.stringify(pickKeys)} is not an \`array\``,
+      );
       return dataSource;
     }
 
-    const obj: Partial<T> = {};
-    if (isArray(pickKeys)) {
-      pickKeys?.forEach((key) => {
-        if (key in dataSource) {
-          obj[key] = dataSource[key];
-        }
-      });
-    } else {
-      console.error(`PickKeys: ${JSON.stringify} 不是个数组`);
-    }
+    const obj: any = {};
+    pickKeys?.forEach((key) => {
+      if (key in dataSource) {
+        obj[key] = dataSource[key];
+      }
+    });
     return obj;
   };
-  static omit = <T extends Darwish.AnyObj>(
+
+  /**
+   * @description omit keys from dataSource 
+   * @param dataSource data source 
+   * @param omitKeys omit keys 
+   * @returns omitted object 
+   */
+  static omit = <T extends Darwish.AnyObj, const K extends keyof T>(
     dataSource: T,
-    omitKeys: (keyof T)[],
-  ) => {
+    omitKeys: K[],
+  ): Omit<T, K> => {
     if (!isObject(dataSource)) {
-      console.error('DataSource 不是一个Object');
+      console.error('DataSource is not an `object`');
+      return dataSource;
+    }
+    if (!isArray(omitKeys)) {
+      console.error(
+        `OmitKeys: ${JSON.stringify(omitKeys)} is not an \`array\``,
+      );
       return dataSource;
     }
 
-    const obj: Partial<T> = {};
-    if (isArray(omitKeys)) {
-      Object.keys(dataSource).forEach((key: keyof T) => {
-        if (!omitKeys.includes(key)) {
-          obj[key] = dataSource[key];
-        }
-      });
-    } else {
-      console.error(`OmitKeys: ${JSON.stringify} 不是个数组`);
-    }
+    const obj: any = {};
+    const keysSet = new Set<keyof T>(omitKeys);
+    Object.keys(dataSource).forEach((key) => {
+      if (!keysSet.has(key)) {
+        obj[key] = dataSource[key];
+      }
+    });
     return obj;
   };
 
+  /**
+   * @description filter useless key-value from dataSource 
+   * @param dataSource data source 
+   * @param isFilterEmptyString is filter empty string 
+   * @returns filtered object 
+   */
   static filterUseless = <T extends Darwish.AnyObj>(
     dataSource: T,
     isFilterEmptyString: boolean = false,
   ) => {
     if (!isObject(dataSource)) {
-      console.error('DataSource 不是一个Object');
+      console.error('DataSource is not an `object`');
       return dataSource;
     }
     const obj: Partial<T> = {};
